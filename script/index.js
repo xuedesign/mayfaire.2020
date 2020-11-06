4e179b88567583193915700dc7c8de5c4b5e4147
xuedesign <elmo@xuedesign.tw>
2020-11-06 19:12:10 +0800

    首頁製作 #20201106

--- script/index.js
+++ script/index.js
@@ -1,16 +1,3 @@
-//menu
-loadjs('https://cdn.jsdelivr.net/npm/superfish@1.7.10/dist/js/superfish.min.js', function() {
-    jQuery(document).ready(function() {
-        jQuery('ul.sf-menu').superfish({
-          cssArrows: false,
-          delay: 0,
-        });
-      });
-});
-
-//menu fixed top
-loadjs("https://xuedesign.github.io/ihp.2020/script/lib/fixed.js");
-
 //banner
 loadjs('https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js', function() {
     $('.main-carousel').flickity({
@@ -35,4 +22,24 @@ loadjs('https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js', function() {
 //wow
 loadjs('https://cdn.rawgit.com/graingert/WOW/34712a3d/dist/wow.min.js', function() {
     new WOW().init();
-});  
+});
+
+//totop
+loadjs(['script/lib/jquery.ui.totop.min.js', 'https://cdn.jsdelivr.net/npm/jquery.easing@1.4.1/jquery.easing.min.js'], function() {
+    $(document).ready(function() {
+	$().UItoTop({ easingType: 'easeOutQuart' });
+    });
+});
+
+// outside link
+function externalLinks() { 
+    if (!document.getElementsByTagName) return; 
+    var anchors = document.getElementsByTagName("a"); 
+    for (var i=0; i<anchors.length; i++) { 
+      var anchor = anchors[i]; 
+      if (anchor.getAttribute("href") && 
+          anchor.getAttribute("rel") == "external") 
+        anchor.target = "_blank"; 
+    } 
+   } 
+   window.onload = externalLinks;
