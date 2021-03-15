73df09d65771c36dfff9c54ad8f775d347f0bf27
ElmoWoo <hello@xuedesign.tw>
2021-03-15 18:26:03 +0800

    首頁製作：行動版選單調整 #20210315

--- script/index.js
+++ script/index.js
@@ -12,7 +12,6 @@ loadjs('https://unpkg.com/flickity/dist/flickity.pkgd.min.js', function() {
         });
 });
 
-
 //lazyload
 loadjs('https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js', function() {
     const observer = lozad();
@@ -25,7 +24,7 @@ loadjs('https://cdn.rawgit.com/graingert/WOW/34712a3d/dist/wow.min.js', function
 });
 
 //totop
-loadjs(['script/lib/jquery.ui.totop.min.js', 'https://cdn.jsdelivr.net/npm/jquery.easing@1.4.1/jquery.easing.min.js'], function() {
+loadjs(['script/lib/jquery.ui.totop.min.js', 'https://cdn.jsdelivr.net/npm/jquery.easing/jquery.easing.min.js'], function() {
     $(document).ready(function() {
 	$().UItoTop({ easingType: 'easeOutQuart' });
     });
